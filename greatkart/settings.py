428381d86bf818039a6d5ee790258ff56d7e31df
vivek kumar <kumarvivek1752@gmail.com>
2022-02-15 09:41:03 +0530

    account functionality is added
    dashboard , activate user, forgot password, is added

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -146,3 +146,13 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+
+
+# SMTP Configration
+
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'eliteblog2292@gmail.com'
+EMAIL_HOST_PASSWORD ='Vivek@9955'
+EMAIL_USE_TLS = True
